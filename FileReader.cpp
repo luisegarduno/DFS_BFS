1e0368eb6535a0562ca520491c661284e9b8839d
luisegarduno <luis.e.garduno@gmail.com>
2020-06-07 08:12:19 -0500

    - Added FileReader class

---
+++ FileReader.cpp
@@ -0,0 +1,172 @@
+//
+// Created by Luis on 6/7/2020.
+//
+
+#include "FileReader.h"
+
+#include <utility>
+
+// Default constructor
+FileReader::FileReader(){
+    numOfFiles = 0;                 // Total number of files (Should be 2)
+    totalDFSreq = 0;                // Total number of Depth First Search request's
+    totalBFSreq = 0;                // Total number of Breadth First Search request's
+
+
+    dfsNode = "";                 // Node for Depth First Search
+    bfsNode = "";                 // Node for Breadth First Search
+    inputFile = "" ;               // Name of inputFile
+    outputFile = "";              // Name of OutputFile
+    controlFile = "";             // Name of file containing all commands
+    makeConnection_1 = "";        // string of first connection (used with mc)
+    makeConnection_2 = "";        // string of first connection (used with mc)
+
+}
+
+void FileReader::setCommands(const char argv[]) {
+    setFileName(argv);
+
+    string word;
+    while (!cfin.eof()) {
+        cfin >> word;
+
+        if(word == "or"){
+            cfin >> word;
+            setInputFile(word);
+        }
+
+        if(word == "ow"){
+            cfin >> word;
+            setOutputFile(word);
+        }
+
+        if(word == "dfs"){
+            cfin >> word;
+            setDfsNode(word);
+            totalDFSreq++;
+        }
+
+        if(word == "bfs"){
+            cfin >> word;
+            setBfsNode(word);
+            totalBFSreq++;
+        }
+
+        if(word == "mc"){
+            cfin >> word;
+            setConnection_1(word);
+
+            cfin >> word;
+            setConnection_2(word);
+        }
+
+        if(word == "dc"){
+            cfin >> word;
+            isDC = true;
+        }
+    }
+
+    closeFiles();
+}
+
+void FileReader::setFileName(const char filename[]){
+    controlFile = filename;
+
+    int outputFilename_size = controlFile.size();
+    string backupControlFile = controlFile + ".txt";
+
+    string file_extension;
+    file_extension += controlFile[outputFilename_size - 4];
+    file_extension += controlFile[outputFilename_size - 3];
+    file_extension += controlFile[outputFilename_size - 2];
+    file_extension += controlFile[outputFilename_size - 1];
+
+    if(file_extension != ".txt"){
+        cfin.open(backupControlFile.c_str());
+        if(!cfin.is_open()){
+            cout << "'" << controlFile << "' or '" << backupControlFile << "' could not be opened. Please check command line." << endl;
+            exit(-1);
+        }
+        controlFile = backupControlFile;
+    }
+    else{
+        cfin.open(controlFile.c_str());
+        if(!cfin.is_open()){
+            cout << "'" << controlFile << "' could not be opened. Please check command line." << endl;
+            exit(-1);
+        }
+    }
+
+}
+
+void FileReader::setInputFile(string file){
+    inputFile = move(file);
+}
+
+string FileReader::getInputFile(){
+    return inputFile;
+}
+
+void FileReader::setOutputFile(string file){
+    outputFile = move(file);
+}
+
+string FileReader::getOutputFile(){
+    return outputFile;
+}
+
+void FileReader::setDfsNode(string node){
+    dfsNode = move(node);
+}
+
+string FileReader::getDfsNode(){
+    return dfsNode;
+}
+
+void FileReader::setBfsNode(string node){
+    bfsNode = move(node);
+}
+
+string FileReader::getBfsNode(){
+    return bfsNode;
+}
+
+void FileReader::setDC(bool aDC){
+    isDC = aDC;
+}
+
+bool FileReader::checkDC(){
+    return isDC;
+}
+
+void FileReader::closeFiles(){
+    cfin.close();
+}
+
+void FileReader::setConnection_1(string connect1){
+    makeConnection_1 = move(connect1);
+}
+
+string FileReader::getConnection_1(){
+    return makeConnection_1;
+}
+
+void FileReader::setConnection_2(string connect2){
+    makeConnection_2 = move(connect2);
+}
+
+string FileReader::getConnection_2(){
+    return makeConnection_2;
+}
+
+int FileReader::getTotalDFSreq(){
+    return totalDFSreq;
+}
+
+int FileReader::getTotalBFSreq(){
+    return totalBFSreq;
+}
+
+
+// FileReader destructor
+FileReader::~FileReader()= default;
---
+++ FileReader.h
@@ -0,0 +1,71 @@
+//
+// Created by Luis on 6/7/2020.
+//
+
+#ifndef DEPTHFIRSTSEARCH_BREADTHFIRST_FILEREADER_H
+#define DEPTHFIRSTSEARCH_BREADTHFIRST_FILEREADER_H
+
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::move;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+
+class FileReader {
+public:
+    // Default constructor
+    FileReader();
+
+    // Used as constructor
+    void setCommands(const char []);
+
+    void setInputFile(string);      // setter for inputFile
+
+
+    string getDfsNode();            // (getter) returns DFS node as string
+    string getBfsNode();            // (getter) returns BFS node as string
+    string getInputFile();          // (getter) returns inputFile
+    string getOutputFile();         // (getter) returns outputFile
+    string getConnection_1();       // (getter) returns makeConnection_1;
+    string getConnection_2();       // (getter) returns makeConnection_2;
+    bool checkDC();
+
+    int getTotalDFSreq();
+    int getTotalBFSreq();
+
+    void closeFiles();              // closes all open files
+
+    ~FileReader();
+
+private:
+    int numOfFiles;                 // Total number of files (Should be 2)
+    int totalDFSreq;                // Total number of Depth First Search request's
+    int totalBFSreq;                // Total number of Breadth First Search request's
+
+    ifstream cfin;                  // Reads in from controlFile;
+
+    string dfsNode;                 // Node for Depth First Search
+    string bfsNode;                 // Node for Breadth First Search
+    string inputFile;               // Name of inputFile
+    string outputFile;              // Name of OutputFile
+    string controlFile;             // Name of file containing all commands
+    string makeConnection_1;        // string of first connection (used with mc)
+    string makeConnection_2;        // string of first connection (used with mc)
+
+    void setDfsNode(string);        // setter for DFS node
+    void setBfsNode(string);        // setter for BFS node
+    void setOutputFile(string);     // setter for outputFile
+    void setConnection_1(string);   // First node for mc
+    void setConnection_2(string);   // Second node for mc
+    void setFileName(const char []);// setter name for ControlFile
+    void setDC(bool);
+    bool isDC;
+
+};
+
+
+#endif //DEPTHFIRSTSEARCH_BREADTHFIRST_FILEREADER_H
